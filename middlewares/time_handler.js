c9ec35ea6ce51fbdec18b6fa37749100a19cf2ea
DwiAmalia <74691354+DwiAmalia@users.noreply.github.com>
2025-07-25 09:37:11 +0700

    time_handler

--- middlewares/time_handler.js
+++ middlewares/time_handler.js
@@ -5,10 +5,20 @@ module.exports = {
     toDate: function (str) {
         return new Date(parseInt(str)).toLocaleDateString("id-ID")
     },
-    toString: function (str) {
-        return new Date(parseInt(str)).toLocaleString("id-ID")
+    // toString: function (str) {
+    //     return new Date(parseInt(str)).toLocaleString("id-ID")
+    // },
+    // toStringFromDate: function (str) {
+    //     return new Date(str).toLocaleString("id-ID")
+    // }
+
+        toString: function (str) {
+        const date = new Date(parseInt(str));
+        return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours().toString().padStart(2, '0')}.${date.getMinutes().toString().padStart(2, '0')}.${date.getSeconds().toString().padStart(2, '0')}`;
     },
+
     toStringFromDate: function (str) {
-        return new Date(str).toLocaleString("id-ID")
+        const date = new Date(str);
+        return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours().toString().padStart(2, '0')}.${date.getMinutes().toString().padStart(2, '0')}.${date.getSeconds().toString().padStart(2, '0')}`;
     }
 }
